1fd34af933158386683aa91d758390778ef4748f
Toreole <toreole.official@gmail.com>
2026-05-29 19:33:01 +0200

    frontend base

---
+++ frontend/src/components/Example.vue
@@ -0,0 +1,21 @@
+<template>
+    <button @click="getNumber">button</button>
+    <p> hier:: {{ count }} ::hier </p>
+</template>
+<script lang="js">
+export default {
+    data() {
+        return {
+            count: 0
+        }
+    }, 
+    methods: {
+        async getNumber() {
+            const response = await fetch("http://localhost:8080/random")
+            if (response.ok) {
+                this.count = await response.json()
+            }
+        }
+    }
+}
+</script>
