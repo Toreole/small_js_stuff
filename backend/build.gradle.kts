62a90e79675962f92524b05828ebe10165af8d85
Toreole <toreole.official@gmail.com>
2026-05-29 21:02:03 +0200

    add openapi generator

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,8 +1,12 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "2.2.21"
     kotlin("plugin.spring") version "2.2.21"
     id("org.springframework.boot") version "4.0.6"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.openapi.generator") version "7.22.0"
+    application
 }
 
 group = "de.tm"
@@ -23,15 +27,56 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // required for generated API services
+    // implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    // implementation("javax.validation:validation-api:2.0.1.Final")
+    //implementation("jakarta.validation:validation-api:3.1.1")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("io.swagger.core.v3:swagger-annotations:2.2.22")
+    implementation("io.swagger.core.v3:swagger-models:2.2.22")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+application {
+    mainClass.set("de.tm.examplewebservice.ExampleWebServiceApplicationKt")
 }
 
 kotlin {
     compilerOptions {
         freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
     }
+    sourceSets["main"].apply {
+        kotlin.srcDirs("src/main/kotlin", "build/generated/src/main/kotlin")
+    }
+}
+
+openApiGenerate {
+    generatorName.set("kotlin-spring")
+    inputSpec.set("$projectDir/api/openapi.yaml")
+    val buildDir = layout.buildDirectory.get()
+    outputDir.set("$buildDir/generated")
+    modelPackage.set("de.tm.example.model")
+    apiPackage.set("de.tm.example.api")
+    configOptions.set(
+        mapOf(
+            "dateLibrary" to "java8",
+            "serviceInterface" to "true",
+            "useTags" to "true",
+            "gradleBuildFile" to "false",
+            "enumPropertyNaming" to "UPPERCASE",
+            "useSpringBoot3" to "true"
+        )
+    )
+    generateApiTests.set(false)
+}
+
+tasks.withType<KotlinCompile> {
+    dependsOn.add(tasks.openApiGenerate)
 }
 
 tasks.withType<Test> {
