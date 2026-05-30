64ce8c707aae0ee289cb482b04d5b13e2e87aaa6
Toreole <toreole.official@gmail.com>
2026-05-30 15:27:19 +0200

    properly use param

--- frontend/src/components/Example.vue
+++ frontend/src/components/Example.vue
@@ -1,5 +1,6 @@
 <template>
     <button @click="getNumber">button</button>
+    <input type="number" v-model="maximum"></input>
     <p> hier:: {{ count }} ::hier </p>
 </template>
 <script lang="js">
@@ -8,12 +9,15 @@ export default {
     data() {
         return {
             count: 0,
-            randomApi: new RandomApi()
+            maximum: 100,
+            randomApi: new RandomApi() 
+            // falls wir einen anderen Server haben
+            // new RandomApi(new ApiClient("http://server.addresse"))
         }
     }, 
     methods: {
         getNumber() {
-            this.randomApi.getRandomNumber(500).then(
+            this.randomApi.getRandomNumber({max: this.maximum}).then(
                 (x) => this.count = x
             )
         }
